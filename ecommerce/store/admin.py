988759a5ad67c2a608e53218502bd08e3a147f3a
Pankaj Kumar <pankajkumardas727@gmail.com>
2021-04-22 19:09:40 +0530

    DB, Models, Render data into cart
    Created DB, models and rendered those on cart.html and checkout.html

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -5,5 +5,5 @@ from .models import *
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
-admin.site.register(Orderitem)
+admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -25,10 +25,10 @@ class Product(models.Model):
             url = self.image.url    
         except:
             url = ''
-        return url        
+        return url 
 
 class Order(models.Model):
-    name = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=200, null=True)
@@ -36,8 +36,19 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
 
-class Orderitem(models.Model):
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
@@ -52,10 +63,11 @@ class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     address = models.CharField(max_length=200, null=True)
+    mobile = models.CharField(max_length=200, null=True)
     city = models.CharField(max_length=200, null=True)
     state = models.CharField(max_length=200, null=True)
     zipcode = models.CharField(max_length=200, null=True)
-    date_added = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.address
