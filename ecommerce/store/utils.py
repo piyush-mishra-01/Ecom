1b5b7ef1981ce49c1e7d6ccb464a6d8152fb547e
piyush-mishra-01 <73028990+piyush-mishra-01@users.noreply.github.com>
2021-04-28 11:49:11 +0530

    Done

--- ecommerce/store/utils.py
+++ ecommerce/store/utils.py
@@ -2,83 +2,93 @@ import json
 from .models import *
 
 def cookieCart(request):
+    try:
+        cart = json.loads(request.COOKIES['cart'])
+    except :
+        cart = {}
 
-	#Create empty cart for now for non-logged in user
-	try:
-		cart = json.loads(request.COOKIES['cart'])
-	except:
-		cart = {}
-		print('CART:', cart)
+    print('Cart: ',cart)
+    items = []
+    order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+    # get_cart_items is a property of Order in models.py
+    cartItems = order['get_cart_items']
 
-	items = []
-	order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
-	cartItems = order['get_cart_items']
+    for i in cart:
+        try:
+            cartItems += cart[i]['quantity']
 
-	for i in cart:
-		#We use try block to prevent items in cart that may have been removed from causing error
-		try:	
-			if(cart[i]['quantity']>0): #items with negative quantity = lot of freebies  
-				cartItems += cart[i]['quantity']
+            product = Product.objects.get(id=i)
+            total = (product.price * cart[i]['quantity'])
 
-				product = Product.objects.get(id=i)
-				total = (product.price * cart[i]['quantity'])
+            order['get_cart_total']+=total
+            order['get_cart_items']+=cart[i]['quantity']
 
-				order['get_cart_total'] += total
-				order['get_cart_items'] += cart[i]['quantity']
+            item = {
+                'product':{
+                    'id':product.id,
+                    'name':product.name,
+                    'price':product.price,
+                    'imageURL':product.imageURL
+                    },
+                'quantity':cart[i]['quantity'],
+                'get_total':total,
+                }
+            items.append(item)
 
-				item = {
-				'id':product.id,
-				'product':{'id':product.id,'name':product.name, 'price':product.price, 
-				'imageURL':product.imageURL}, 'quantity':cart[i]['quantity'],
-				'digital':product.digital,'get_total':total,
-				}
-				items.append(item)
+            if product.digital == False:
+                order['shipping'] = True
+        except:
+            pass
+    return {'cartItems':cartItems, 'order':order, 'items':items}
 
-				if product.digital == False:
-					order['shipping'] = True
-		except:
-			pass
-			
-	return {'cartItems':cartItems ,'order':order, 'items':items}
 
 def cartData(request):
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		items = order.orderitem_set.all()
-		cartItems = order.get_cart_items
-	else:
-		cookieData = cookieCart(request)
-		cartItems = cookieData['cartItems']
-		order = cookieData['order']
-		items = cookieData['items']
+    # giving the same code as cart, bcz same, total data will be rendered in frontend
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        # get_or_create is used to search for a qiven object,
+        # and, if it isn't there, it then creates tht model object
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        # the below line, is used to query from the OrderItem model
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        # cookieCart function is present in utils.py, and to use it, it's imported here
+        # check utils.py for cookieCart function
+        cookieData = cookieCart(request)
+        # cookieData is a dictionary
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
+    return {'cartItems':cartItems, 'order':order, 'items':items}
 
-	return {'cartItems':cartItems ,'order':order, 'items':items}
 
-	
 def guestOrder(request, data):
-	name = data['form']['name']
-	email = data['form']['email']
+    print("User is not logged in..")
 
-	cookieData = cookieCart(request)
-	items = cookieData['items']
+    print("COOKIES:", request.COOKIES)
+    name = data['form']['name']
+    email = data['form']['email']
 
-	customer, created = Customer.objects.get_or_create(
-			email=email,
-			)
-	customer.name = name
-	customer.save()
+    cookieData = cookieCart(request)
+    items = cookieData['items']
 
-	order = Order.objects.create(
-		customer=customer,
-		complete=False,
-		)
+    customer, created  = Customer.objects.get_or_create(
+        email = email,
+        )
+    customer.name = name
+    customer.save()
 
-	for item in items:
-		product = Product.objects.get(id=item['id'])
-		orderItem = OrderItem.objects.create(
-			product=product,
-			order=order,
-			quantity=(item['quantity'] if item['quantity']>0 else -1*item['quantity']), # negative quantity = freebies
-		)
-	return customer, order
+    order = Order.objects.create(
+        customer = customer,
+        complete = False,
+        )
+
+    for item in items:
+        product = Product.objects.get(id=item['product']['id'])
+        orderItem = OrderItem.objects.create(
+            product = product,
+            order = order,
+            quantity = item['quantity']
+            )
+    return customer, order
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,92 +1,107 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-import json
 import datetime
-from .models import * 
-from .utils import cookieCart, cartData, guestOrder
+import json
+from .models import  *
+from .utils import *
+
+
+# remember to change the code here, to show total cart value at about page too
+def about(request):
+    data  = cartData(request)
+    cartItems = data['cartItems']
+
+    products = Product.objects.all()
+    context = {"products":products, 'cartItems':cartItems}
+    return render(request, 'store/about.html', context)
 
 def store(request):
-	data = cartData(request)
+    data  = cartData(request)
+    cartItems = data['cartItems']
 
-	cartItems = data['cartItems']
-	order = data['order']
-	items = data['items']
-
-	products = Product.objects.all()
-	context = {'products':products, 'cartItems':cartItems}
-	return render(request, 'store/store.html', context)
+    products = Product.objects.all()
+    context = {"products":products, 'cartItems':cartItems}
+    return render(request, 'store/store.html', context)
 
 
 def cart(request):
-	data = cartData(request)
+    data  = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
 
-	cartItems = data['cartItems']
-	order = data['order']
-	items = data['items']
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
+    return render(request, 'store/cart.html', context)
 
-	context = {'items':items, 'order':order, 'cartItems':cartItems}
-	return render(request, 'store/cart.html', context)
 
 def checkout(request):
-	data = cartData(request)
-	
-	cartItems = data['cartItems']
-	order = data['order']
-	items = data['items']
+    data  = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
 
-	context = {'items':items, 'order':order, 'cartItems':cartItems}
-	return render(request, 'store/checkout.html', context)
+    context = {'items':items, 'order':order, 'cartItems':cartItems, 'shipping':False}
+    return render(request, 'store/checkout.html', context)
+
 
 def updateItem(request):
-	data = json.loads(request.body)
-	productId = data['productId']
-	action = data['action']
-	print('Action:', action)
-	print('Product:', productId)
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action :', action)
+    print('productId :',productId)
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    # below line attaches the order to the given customer
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    # in the below line, we r using 'get_or_create' to change the values of orderItem, if it already exists
+    # so, if it already exists, we don't wan't to create orderItem again, we just want to change the quantity of orderItem
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
 
-	customer = request.user.customer
-	product = Product.objects.get(id=productId)
-	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    if action =='add':
+        # by clicking up arrow, increment orderItem by 1
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        # by clicking up arrow, decrement orderItem by 1
+        orderItem.quantity = (orderItem.quantity - 1)
 
-	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+    # save quantity of products, for an order
+    orderItem.save()
 
-	if action == 'add':
-		orderItem.quantity = (orderItem.quantity + 1)
-	elif action == 'remove':
-		orderItem.quantity = (orderItem.quantity - 1)
+    if orderItem.quantity <= 0:
+        # remove the orderItem from cart, when quantity reaches 0, or below it
+        orderItem.delete()
 
-	orderItem.save()
+    return JsonResponse('Item was added', safe=False)
 
-	if orderItem.quantity <= 0:
-		orderItem.delete()
-
-	return JsonResponse('Item was added', safe=False)
 
 def processOrder(request):
-	transaction_id = datetime.datetime.now().timestamp()
-	data = json.loads(request.body)
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
 
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-	else:
-		customer, order = guestOrder(request, data)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
 
-	total = float(data['form']['total'])
-	order.transaction_id = transaction_id
+    else:
+        # guestOrder function is present in utils.py
+        customer, order = guestOrder(request, data)
 
-	if total == order.get_cart_total:
-		order.complete = True
-	order.save()
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
 
-	if order.shipping == True:
-		ShippingAddress.objects.create(
-		customer=customer,
-		order=order,
-		address=data['shipping']['address'],
-		city=data['shipping']['city'],
-		state=data['shipping']['state'],
-		zipcode=data['shipping']['zipcode'],
-		)
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
 
-	return JsonResponse('Payment submitted..', safe=False)
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer = customer,
+            order = order,
+            address = data['shipping']['address'],
+            city = data['shipping']['city'],
+            state = data['shipping']['state'],
+            zipcode = data['shipping']['zipcode'],
+            )
+
+    return JsonResponse("Payment submitted...",safe=False)
